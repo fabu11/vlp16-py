2f392cda8685243b2efc5aaa6bbb2933857eef5f
fabu11 <abullarade.franco@gmail.com>
2025-02-04 18:30:55 -0800

    adjusting based on range

--- vlp_subscriber.py
+++ vlp_subscriber.py
@@ -21,9 +21,17 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py.point_cloud2 import read_points
 import time, os, math
 
+cnt = 0
+
 class MinimalSubscriber(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
+        # Shortest distance used in sort
+        self.shortest_distance = {               
+                        "range": 9999,
+                        "index": 0,
+                        }
+        self.storage = {}
         self.subscription = self.create_subscription(
             PointCloud2,
             'velodyne_points',
@@ -39,16 +47,25 @@ class MinimalSubscriber(Node):
     def calculate_range(self, x, y, z):
         return math.sqrt(x**2 + y**2 + z**2)
 
-
     def listener_callback(self, msg):
-        points = read_points(cloud=msg, field_names=('x', 'y', 'z'), skip_nans=False) # pyright: ignore
-        point_array = np.array([[self.calculate_range(p[0], p[1], p[2]),
-                                 p[0], p[1], p[2]] for p in points], dtype=np.float32)
-        point_array[np.isnan(point_array)] = 999.0
+        points = list(read_points(cloud=msg, field_names=('x', 'y', 'z', 'intensity'), skip_nans=False)) # pyright: ignore
+        for i, p in enumerate(points):
+            # Distance is shorter, need to update origin
+            c = self.calculate_range(p[0], p[1], p[2]) 
+            if(c < self.shortest_distance["range"]):
+                new_shortest = {
+                        "range": c,
+                        "index": i,
+                        }
+                self.shortest_distance = new_shortest
+        new_p = points[self.shortest_distance["index"]:] + points[:self.shortest_distance["index"]]
+        new_p = np.array(new_p)
+        np.nan_to_num(new_p, nan=0)
         timestamp = int(time.time() * 1000)
         filename = f'{self.directory}/scan_{timestamp}.txt'
-        np.savetxt(filename, point_array, fmt='%.4f', header='x\ty\tz')
-        self.get_logger().info(f'Saved point cloud to {filename} with {point_array.shape[0]} points')
+        np.savetxt(filename, new_p, fmt='%.4f', header='x\ty\tz')
+        self.get_logger().info(f'Saved point cloud to {filename} with {len(new_p)} points')
+        
 
 def main(args=None):
     print("start")
